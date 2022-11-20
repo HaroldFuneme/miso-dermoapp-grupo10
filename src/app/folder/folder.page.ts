5837fa7abf750e6099f59ae17ae081ab5a879cee
Leonardo Garzon <l.garzonr@unianes.edu.co>
2022-11-20 12:53:17 -0500

    pruebas de integracion cognito

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { Auth } from 'aws-amplify';
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
@@ -9,10 +9,34 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
+  async createUser() {
+    console.log('twefasvd');
+    try {
+      const { user } = await Auth.signUp({
+        username: 'Leonardo',
+        password: 'LeonardoGarzon92%',
+        attributes: {
+          email: 'l.garzonr@uniandes.edu.co', // required
+        },
+      });
+      console.log(user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async signIn() {
+    try {
+      const user = await Auth.signIn('Leonardo', 'LeonardoGarzon92%');
+      console.log(user)
+    } catch (error) {
+      console.log('error signing in', error);
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { Amplify, Auth } from 'aws-amplify';
+import { awsconfig } from './config/awsconfig';
+
+Amplify.configure(awsconfig);
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
