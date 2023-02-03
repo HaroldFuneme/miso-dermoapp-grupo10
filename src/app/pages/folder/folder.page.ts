3465b7a9a8c81ddf435623c2a6b27e78160258a2
Harold Funeme <h.funeme@uniandes.edu.co>
2023-02-02 19:40:40 -0500

    🐧 FINISH  of internationalization implementation i18n

--- src/app/pages/folder/folder.page.ts
+++ src/app/pages/folder/folder.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Geolocation, Position } from '@capacitor/geolocation';
 import { DemoService } from '../../services/demo.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-folder',
@@ -12,8 +13,14 @@ export class FolderPage implements OnInit {
   public folder: string;
   public url: string;
   public position: Position;
+  public lagns: string [] = [];
 
-  constructor(private activatedRoute: ActivatedRoute, public demoService: DemoService) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    public demoService: DemoService,
+    private translateService: TranslateService) {
+      this.lagns = translateService.getLangs();
+    }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
@@ -28,6 +35,12 @@ export class FolderPage implements OnInit {
       this.url = `https://www.google.com/maps/search/?api=1&query=${coordinates.coords.latitude}%2C${coordinates.coords.longitude}`;
     };
     printCurrentPosition();
+  }
+
+  changeLang(event){
+    console.log(event);
+    this.translateService.use(event.detail.value);
+
   }
 
 }
