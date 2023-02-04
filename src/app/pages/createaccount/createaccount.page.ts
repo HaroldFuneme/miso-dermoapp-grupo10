19f3a87a4f6e7c18ead90de02de0d95815877899
Harold Funeme <h.funeme@uniandes.edu.co>
2023-02-04 10:31:29 -0500

    🐧 update translate :pencil:

--- src/app/pages/createaccount/createaccount.page.ts
+++ src/app/pages/createaccount/createaccount.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-createaccount',
@@ -11,23 +12,48 @@ export class CreateaccountPage implements OnInit {
 
   constructor(
     private alertController: AlertController,
-    private router: Router
+    private router: Router,
+    private translateService: TranslateService
     ) { }
 
   ngOnInit() {
   }
 
   async presentAlert() {
-    const alert = await this.alertController.create({
-      header: 'Alert',
-      subHeader: 'Important message',
-      message: 'In order to use our App and/or receive our Services, \
+    console.log(this.translateService.currentLang);
+    let alertLangMessage: string;
+    let alertSubHeader: string;
+    let alertHeader: string;
+    const alertButtons: string [] = [];
+
+
+
+    if (this.translateService.currentLang ==='Español') {
+      alertHeader = 'Alerta';
+      alertSubHeader = '¡Mensaje Importante!';
+      alertLangMessage = 'Para utilizar nuestra aplicación y/o recibir nuestros servicios, \
+      debe tener al menos 18 años de edad, o la mayoría de edad legal \
+      en su jurisdicción, y posee la autoridad legal, el derecho y la libertad \
+      para celebrar estos Términos como un acuerdo vinculante. . No estas permitido \
+      para usar esta aplicación y / o recibir servicios si está haciendo \
+      así está prohibido en su país o bajo cualquier ley o regulación aplicable a usted.';
+      alertButtons.push('Aceptar');
+    }else{
+      alertHeader = 'Alert';
+      alertSubHeader = 'Important message';
+      alertLangMessage = 'In order to use our App and/or receive our Services, \
       you must be at least 18 years of age, or the legal age of majority \
       in your jurisdiction, and possess the legal authority, right, and liberty \
       to enter into these Terms as a binding agreement. . You are not allowed \
       to use this application and / or receive services if doing \
-      so is prohibited in your country or under any law or regulation applicable to you.',
-      buttons: ['Accept'],
+      so is prohibited in your country or under any law or regulation applicable to you.';
+      alertButtons.push('Accept');
+    }
+    const alert = await this.alertController.create({
+      header: alertHeader,
+      subHeader: alertSubHeader,
+      message: alertLangMessage,
+      buttons: alertButtons,
     });
 
     await alert.present();
