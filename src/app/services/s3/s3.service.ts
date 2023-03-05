e9d83ebb197230da4aaa8992ffac7a4693e8bff8
Harold Funeme <h.funeme@uniandes.edu.co>
2023-03-05 09:41:55 -0500

    🐧 add upload img success

--- src/app/services/s3/s3.service.ts
+++ src/app/services/s3/s3.service.ts
@@ -12,8 +12,10 @@ export class S3Service {
 
   s3(){
     const s3 = new AWS.S3({
-      accessKeyId: 'AKIAQLDZX6ITGXD6Q4XW',
-      secretAccessKey: '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'
+    accessKeyId: 'AKIAQLDZX6ITGXD6Q4XW',
+    secretAccessKey: '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'
+    //  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    //  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     });
     return s3;
   }
